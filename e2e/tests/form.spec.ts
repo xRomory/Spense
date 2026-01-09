57ed6404632dde605d28cd00306bf9aefc7cd5a7
chayen <romory@pop-os.CG-24D>
2026-01-09 16:07:49 +0800

    Fixed Test case when creatingan  Expense

--- e2e/tests/form.spec.ts
+++ e2e/tests/form.spec.ts
@@ -16,6 +16,7 @@ test("user can create new group", async ({ page }) => {
 test("user can add an expense", async ({ page }) => {
   // Create a group first
   await page.goto("http://localhost:3000");
+  await page.evaluate(() => localStorage.clear());
 
   // Fill out the form
   await page.fill('input[placeholder="e.g. Room 123, Trip to Boracay"]', 'Test Group');
@@ -24,22 +25,21 @@ test("user can add an expense", async ({ page }) => {
   // Submit form
   await page.click('button:has-text("Create Group")');
 
+  // Ensure we are on /app
+  await page.waitForURL(/\/app$/);
+
   // Go to the "Add Expense tab"
-  await page.click("text=Add Expense");
+  await page.getByRole('tab', { name: 'Add Expense' }).click();
 
   // Fill out the form
   await page.fill('input[placeholder="e.g. Electric Bill - September"]', 'Test Expense');
   await page.fill('input[placeholder="₱0.00"]', '1504.00');
 
   await page.click('button:has-text("Select who paid")');
-
-  // Wait for the dropdown to appear
-  await expect(page.locator('[role="option"]:has-text("Test User")')).toBeVisible();
-
-  await page.click('[role="option"]:has-text("Test User")');
+  await page.getByRole('option', { name: 'Test User' }).click();
 
   // Submit the form
-  await page.click('button:has-text("Add Expense")');
+  await page.getByRole('button', { name: 'Add Expense' }).click();
 
   // Assert the expense appears in the list
   await page.click('text=All Expenses');
